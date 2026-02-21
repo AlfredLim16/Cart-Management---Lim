e82c8d3408baee0d03d2edae65ac9c880e0a531e
Alfred Lim <alfredmanelim@gmail.com>
2026-02-21 11:38:34 +0800

    Initial commit

---
+++ Cart Management/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Cart_Management
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {                    
+            int choice = 0;
+            while(choice != 3)
+            {
+                switch (choice)
+                {
+                    case 0:
+                        addToCart();
+                    break;
+                    case 1:
+                        Console.WriteLine("View Cart");
+                    break;
+                    case 2:
+                        Console.WriteLine("");
+                    break;
+                    case 3:
+                        Console.WriteLine("Exit");
+                    break;
+                }
+            }
+        }
+
+        static void addToCart()
+        {
+            string[] items = new string[3];
+            string[] prices = new string[3];
+            items[0] = "1. Apple";
+            items[1] = "2. Banana";
+            items[2] = "3. Watermelon";
+            prices[0] = "1. 50.00";
+            prices[1] = "2. 30.00";
+            prices[2] = "3. 100.00";
+
+            foreach (string item in items)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.Write("Select an Item: ");
+            string selectItem = Console.ReadLine();
+
+            string userChoice = "yes";
+
+            Console.WriteLine("Do you want to add this item into cart? (yes/no): ");
+            userChoice = Console.ReadLine();
+
+            
+            if (userChoice == "yes")
+            {
+                Console.WriteLine("Item added to cart!!");
+            }
+            else
+            {
+                Console.WriteLine("Item failed to add to cart!!");
+            }
+        }
+    }
+}
